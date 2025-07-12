5de143d0c55d44c0b55cf2f5a44dcbc87aa9d4df
George Malak <georgmalak2004@gmail.com>
2025-07-12 11:20:44 +0300

    Edit

--- Tasks/T11/src/components/protectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, useLocation } from "react-router-dom";
-
-export default function ProtectedRoute({isAllowed , redirectPath , children}){
-    const location = useLocation()
-    if(!isAllowed()){
-        return <Navigate to={redirectPath} state={{from:location.pathname}}/>
-    }
-    return children
-}
